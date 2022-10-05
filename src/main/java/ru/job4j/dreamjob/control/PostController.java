5d23dbedc255539492ed00985e476d65ca1c23bb
Vitaliy123 <cpc1251@mail.ru>
2022-10-05 21:44:54 +0300

    job4j-82 4. Thymeleaf, Циклы. [#504841]
    1. Создал модель Post. Добавил в нее поля: ID, name, description, created.
    2. Создал хранилище PostStore. Добавил в него три объекта Post.
    3. Cоздал контроллер PostController.
    4. Создал вид posts.html. Вид выводит таблицу Post.

---
+++ src/main/java/ru/job4j/dreamjob/control/PostController.java
@@ -0,0 +1,22 @@
+package ru.job4j.dreamjob.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.store.PostStore;
+
+/**
+ * Created by Oywayten on 05.10.2022.
+ * Класс-контроллер.
+ */
+@Controller
+public class PostController {
+
+    private final PostStore store = PostStore.instOf();
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        model.addAttribute("posts", store.findAll());
+        return "posts";
+    }
+}
