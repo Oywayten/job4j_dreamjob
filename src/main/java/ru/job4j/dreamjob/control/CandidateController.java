7de5c4fdf02be4d75f7b25e502fe1a4e27b6a38e
Vitaliy123 <cpc1251@mail.ru>
2022-10-07 22:05:24 +0300

    job4j-86 8. HTML form. Создание кандидата. [#504845]
    Создал MVС для кандидата: CandidateController.addCandidat, addCandidate.html;
    Добавил форму ввода кандидата;
    Добавил на страницу index.html ссылку на страницу создания кандидата.

--- src/main/java/ru/job4j/dreamjob/control/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/control/CandidateController.java
@@ -3,6 +3,7 @@ package ru.job4j.dreamjob.control;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.CandidateStore;
 
 /**
@@ -18,5 +19,11 @@ public class CandidateController {
     public String candidates(Model model) {
         model.addAttribute("candidates", store.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/formAddCandidate")
+    public String addCandidate(Model model) {
+        model.addAttribute("candidate", new Candidate(0, "Заполните поле", "Заполните поле", "Заполните поле"));
+        return "addCandidate";
     }
 }
