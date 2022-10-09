7f334c22bedf8b8798b4747b2c62ebe325db2ef6
Vitaliy123 <cpc1251@mail.ru>
2022-10-09 20:48:30 +0300

    job4j-90 3. PostController.createPost. Создание вакансии. [#504849]
    Объеденил пакеты контроллеров;
    Добавил использование AtomicInteger;

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Oywayten on 05.10.2022.
@@ -16,11 +17,12 @@ public class PostStore {
     private static final PostStore INST = new PostStore();
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger(1);
 
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "This is Junior Java Job", "2022-01-01"));
-        posts.put(2, new Post(2, "Middle Java Job", "This is Middle Java Job", "2022-01-02"));
-        posts.put(3, new Post(3, "Senior Java Job", "This is Senior Java Job", "2022-01-03"));
+        posts.put(id.getAndIncrement(), new Post(1, "Junior Java Job", "This is Junior Java Job", "2022-01-01"));
+        posts.put(id.getAndIncrement(), new Post(2, "Middle Java Job", "This is Middle Java Job", "2022-01-02"));
+        posts.put(id.getAndIncrement(), new Post(3, "Senior Java Job", "This is Senior Java Job", "2022-01-03"));
     }
 
     public static PostStore instOf() {
@@ -36,10 +38,9 @@ public class PostStore {
     }
 
     public void add(Post post) {
-        int newId = posts.size() + 1;
-        post.setId(newId);
+        post.setId(id.getAndIncrement());
         post.setCreated(LocalDate.now().toString());
-        posts.put(newId, post);
+        posts.put(post.getId(), post);
     }
 
     public void update(Post post) {
