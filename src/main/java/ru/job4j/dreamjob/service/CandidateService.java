b4a4d7eb4935585b0bd48f70996728ac71383fe7
Vitaliy123 <cpc1251@mail.ru>
2022-10-11 00:11:20 +0300

    job4j-93 1. Слоеная архитектура. [#504851]
    Организовал слои в проложении: control используют service, service используют persistence.

---
+++ src/main/java/ru/job4j/dreamjob/service/CandidateService.java
@@ -0,0 +1,37 @@
+package ru.job4j.dreamjob.service;
+
+import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.persistence.CandidateStore;
+
+import java.util.Collection;
+
+/**
+ * Created by Oywayten on 10.10.2022.
+ */
+public class CandidateService {
+    private static final CandidateService INST = new CandidateService();
+    private final CandidateStore store = CandidateStore.instOf();
+
+    private CandidateService() {
+    }
+
+    public static CandidateService instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return store.findAll();
+    }
+
+    public Candidate findById(int id) {
+        return store.findById(id);
+    }
+
+    public void add(Candidate candidate) {
+        store.add(candidate);
+    }
+
+    public void update(Candidate candidate) {
+        store.update(candidate);
+    }
+}
