53df064d572cc3ce289fb9373bbad1e51c2142f6
Vitaliy123 <cpc1251@mail.ru>
2022-10-14 20:55:07 +0300

    job4j-100 1. Подключение к базе в веб приложении. Хранение вакансий. [#504859]
    Вынес создание объектов в приватный метод.
    Запросы к базе данных перенес в константы на уровне класса.
    Поменял подключение логгера.

--- src/main/java/ru/job4j/dreamjob/persistence/CandidateDBStore.java
+++ src/main/java/ru/job4j/dreamjob/persistence/CandidateDBStore.java
@@ -1,8 +1,8 @@
 package ru.job4j.dreamjob.persistence;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
@@ -16,8 +16,12 @@ import java.util.List;
  */
 @Repository
 public class CandidateDBStore {
-    private static final Logger LOG = LoggerFactory.getLogger(CandidateDBStore.class);
-
+    private static final String FIND_ALL = "SELECT * FROM candidate";
+    private static final String ADD = "INSERT INTO candidate(fname, description, created, file) VALUES (?, ?, ?, ?)";
+    private static final String UPDATE =
+            "UPDATE candidate SET fname = ?, description = ?, created = ?, file = ? WHERE id = ?";
+    private static final String FIND_BY_ID = "SELECT * FROM candidate WHERE id = ?";
+    private static final Logger LOG = LogManager.getLogger(CandidateDBStore.class);
     private final BasicDataSource pool;
 
     public CandidateDBStore(BasicDataSource pool) {
@@ -27,26 +31,26 @@ public class CandidateDBStore {
     public List<Candidate> findAll() {
         List<Candidate> candidates = new ArrayList<>();
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidate")
-        ) {
+             PreparedStatement ps = cn.prepareStatement(FIND_ALL)) {
             try (ResultSet it = ps.executeQuery()) {
                 while (it.next()) {
-                    candidates.add(new Candidate(it.getInt("id"), it.getString("fname"), it.getString("description"),
-                            it.getTimestamp("created").toLocalDateTime(), it.getBytes("file")));
+                    candidates.add(getCandidate(it));
                 }
             }
         } catch (Exception e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
         return candidates;
     }
 
+    private Candidate getCandidate(ResultSet it) throws SQLException {
+        return new Candidate(it.getInt("id"), it.getString("fname"), it.getString("description"),
+                it.getTimestamp("created").toLocalDateTime(), it.getBytes("file"));
+    }
+
     public Candidate add(Candidate candidate) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement(
-                     "INSERT INTO candidate(fname, description, created, file) VALUES (?, ?, ?, ?)",
-                     PreparedStatement.RETURN_GENERATED_KEYS)
-        ) {
+             PreparedStatement ps = cn.prepareStatement(ADD, PreparedStatement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, candidate.getName());
             ps.setString(2, candidate.getDesc());
             ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
@@ -58,16 +62,14 @@ public class CandidateDBStore {
                 }
             }
         } catch (Exception e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
         return candidate;
     }
 
     public void update(Candidate candidate) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement(
-                     "UPDATE candidate SET fname = ?, description = ?, created = ?, file = ? WHERE id = ?"
-             )) {
+             PreparedStatement ps = cn.prepareStatement(UPDATE)) {
             ps.setString(1, candidate.getName());
             ps.setString(2, candidate.getDesc());
             ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
@@ -75,23 +77,21 @@ public class CandidateDBStore {
             ps.setInt(5, candidate.getId());
             ps.execute();
         } catch (SQLException e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
     }
 
     public Candidate findById(int id) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidate WHERE id = ?")
-        ) {
+             PreparedStatement ps = cn.prepareStatement(FIND_BY_ID)) {
             ps.setInt(1, id);
             try (ResultSet it = ps.executeQuery()) {
                 if (it.next()) {
-                    return new Candidate(it.getInt("id"), it.getString("fname"), it.getString("description"),
-                            it.getTimestamp("created").toLocalDateTime(), it.getBytes("file"));
+                    return getCandidate(it);
                 }
             }
         } catch (Exception e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
         return null;
     }
--- src/main/java/ru/job4j/dreamjob/persistence/PostDBStore.java
+++ src/main/java/ru/job4j/dreamjob/persistence/PostDBStore.java
@@ -1,7 +1,7 @@
 package ru.job4j.dreamjob.persistence;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.stereotype.Repository;
@@ -18,7 +18,13 @@ import java.util.List;
  */
 @Repository
 public class PostDBStore {
-    private static final Logger LOG = LoggerFactory.getLogger(PostDBStore.class);
+    private static final String FIND_ALL = "SELECT * FROM post";
+    private static final String ADD =
+            "INSERT INTO post(fname, description, created, visible, city_id) VALUES (?, ?, ?, ?,?)";
+    private static final String UPDATE =
+            "UPDATE post SET fname = ?, description = ?, created = ?, visible = ?, city_id = ? WHERE id = ?";
+    private static final String FIND_BY_ID = "SELECT * FROM post WHERE id = ?";
+    private static final Logger LOG = LogManager.getLogger(PostDBStore.class);
 
     private final BasicDataSource pool;
 
@@ -29,27 +35,27 @@ public class PostDBStore {
     public List<Post> findAll() {
         List<Post> posts = new ArrayList<>();
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement("SELECT * FROM post")
-        ) {
+             PreparedStatement ps = cn.prepareStatement(FIND_ALL)) {
             try (ResultSet it = ps.executeQuery()) {
                 while (it.next()) {
-                    posts.add(new Post(it.getInt("id"), it.getString("fname"), it.getString("description"),
-                            it.getTimestamp("created").toLocalDateTime(), it.getBoolean("visible"),
-                            new City(it.getInt("city_id"), null)));
+                    posts.add(getPost(it));
                 }
             }
         } catch (Exception e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
         return posts;
     }
 
+    private Post getPost(ResultSet it) throws SQLException {
+        return new Post(it.getInt("id"), it.getString("fname"), it.getString("description"),
+                it.getTimestamp("created").toLocalDateTime(), it.getBoolean("visible"),
+                new City(it.getInt("city_id"), null));
+    }
+
     public Post add(Post post) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement(
-                     "INSERT INTO post(fname, description, created, visible, city_id) VALUES (?, ?, ?, ?,?)",
-                     PreparedStatement.RETURN_GENERATED_KEYS)
-        ) {
+             PreparedStatement ps = cn.prepareStatement(ADD, PreparedStatement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, post.getName());
             ps.setString(2, post.getDescription());
             ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
@@ -62,16 +68,14 @@ public class PostDBStore {
                 }
             }
         } catch (Exception e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
         return post;
     }
 
     public void update(Post post) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement(
-                     "UPDATE post SET fname = ?, description = ?, created = ?, visible = ?, city_id = ? WHERE id = ?"
-             )) {
+             PreparedStatement ps = cn.prepareStatement(UPDATE)) {
             ps.setString(1, post.getName());
             ps.setString(2, post.getDescription());
             ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
@@ -80,24 +84,21 @@ public class PostDBStore {
             ps.setInt(6, post.getId());
             ps.execute();
         } catch (SQLException e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
     }
 
     public Post findById(int id) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement("SELECT * FROM post WHERE id = ?")
-        ) {
+             PreparedStatement ps = cn.prepareStatement(FIND_BY_ID)) {
             ps.setInt(1, id);
             try (ResultSet it = ps.executeQuery()) {
                 if (it.next()) {
-                    return new Post(it.getInt("id"), it.getString("fname"), it.getString("description"),
-                            it.getTimestamp("created").toLocalDateTime(), it.getBoolean("visible"),
-                            new City(it.getInt("city_id"), null));
+                    return getPost(it);
                 }
             }
         } catch (Exception e) {
-            LOG.trace("Exception in log: ", e);
+            LOG.error(e.getMessage(), e);
         }
         return null;
     }
