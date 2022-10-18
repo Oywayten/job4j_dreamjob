f69604a46b1c91458d030b2471d4b14e6d85347c
Vitaliy123 <cpc1251@mail.ru>
2022-10-18 19:07:23 +0300

    job4j-108 3. Filter [#504865]
    Дополнил список постфиксов страниц разрешенных для доступа без аутентификации.
    Добавил меню на страницу с логином для удобства переключения между страницами.

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -6,12 +6,23 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 /**
  * Created by Oywayten on 16.10.2022.
+ * Класс-фильтр производит проверку доступа.
  */
 @Component
 public class AuthFilter implements Filter {
+    /**
+     * Список постфиксов страниц разрешенных для доступа без аутентификации.
+     */
+    private final Set<String> allowedPageSet = Set.of(
+            "loginPage",
+            "login",
+            "addUser",
+            "registration",
+            "index");
 
     @Override
     public void doFilter(
@@ -19,7 +30,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login")) {
+        if (pageCheck(allowedPageSet, uri)) {
             chain.doFilter(req, res);
             return;
         }
@@ -28,5 +39,9 @@ public class AuthFilter implements Filter {
             return;
         }
         chain.doFilter(req, res);
+    }
+
+    private boolean pageCheck(Set<String> pageSet, String uri) {
+        return pageSet.stream().anyMatch(uri::endsWith);
     }
 }
